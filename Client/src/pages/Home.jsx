349c7a553c7ed27b73ff412d54ccc7b03d3fc703
Saumy1312 <saumysingh60@gmail.com>
2025-08-11 03:00:48 +0530

    file name changed

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from '../components/Hero'
 import Aitoole from '../components/Aitoole'
 import Testimonial from '../components/Testimonial'
 import Plan from '../components/Plan'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
